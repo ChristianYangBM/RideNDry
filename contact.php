48ac0ca10ed6a0128645a1ff563b29f240124529
byteMonsterMicah <micahrandolph@bytemonster.co>
2025-06-09 21:57:06 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,7 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 $config = require '../private/mail_config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
